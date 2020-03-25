29865cd7f7b21a7283ed03465238cf653b5b1eac
hiba1996-stack <hibaattia1@gmail.com>
2020-03-25 22:26:18 +0100

    edit3

--- src/main/java/com/HRPlus/space/entities/NoteInterne.java
+++ src/main/java/com/HRPlus/space/entities/NoteInterne.java
@@ -1,11 +1,16 @@
 package com.HRPlus.space.entities;
 
 import java.sql.Date;
+import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,6 +25,22 @@ public class NoteInterne {
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id ; 
+	@NotBlank
+    @Column
 	private String title ; 
+	@Lob
+    @Column
+    @NotEmpty
 	private String content ; 
+	@Column
+	private Instant createdOn ; 
+	@Column
+	private Instant updatedOn ; 
+	@Column
+	@NotBlank
+	private String username ; 
+	
+	public void setUsername(String username) {
+        this.username = username;
+    }
 }
